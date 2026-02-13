455772ab75b75a962e9e2198678652dd99dda96f
rodrigo <rodrigochoquebautista000@gmail.com>
2026-02-13 19:08:01 -0400

    add: generate sprintboot proyect

---
+++ src/main/java/com/tiendarich/gestion_tienda/GestionTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.tiendarich.gestion_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendarich/gestion_tienda/GestionTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendarich.gestion_tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
